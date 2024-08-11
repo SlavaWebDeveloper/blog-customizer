1756a88523af3e438772d4cd84cf50f1ac5d3188
Slava Sidorov <sidorov-vv3@mail.ru>
2024-08-11 21:13:20 +0700

    Update index file. Make dir app and transfer code from index.tsx to app.tsx

---
+++ src/components/app/app.tsx
@@ -0,0 +1,31 @@
+import React, { CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+import styles from './app.module.scss';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+import { ArticleParamsForm } from '../article-params-form';
+import { Article } from '../article';
+
+export const App = () => {
+	const [appState, setAppState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': appState.fontFamilyOption.value,
+					'--font-size': appState.fontSizeOption.value,
+					'--font-color': appState.fontColor.value,
+					'--container-width': appState.contentWidth.value,
+					'--bg-color': appState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm setAppState={setAppState} />
+			<Article />
+		</main>
+	);
+};
